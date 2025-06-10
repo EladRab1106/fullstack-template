be804c242351928128224e9f76aa4fe8d489af3f
EladRab1106 <eladrabinovitch1106@gmail.com>
2025-06-10 19:22:15 +0300

    final

--- server/src/index.js
+++ server/src/index.js
@@ -1,11 +1,13 @@
-const express = require("express");
-const cors = require("cors");
+const express = require('express');
+const cors = require('cors');
 const app = express();
+require('dotenv').config();
 app.use(cors());
+app.use(express.json());
 const PORT = process.env.PORT || 3001;
 
-app.get("/", (req, res) => {
-  res.send("Hello from backend!");
+app.get('/', (req, res) => {
+  res.send('Hello from backend!');
 });
 
 app.listen(PORT, () => {
